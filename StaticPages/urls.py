85b18536bd222a9690134500398a8cdd1d1a3f33
Tom Crossley <tcac2@cam.ac.uk>
2019-08-08 23:53:15 +0100

    Basic Skeleton Site: Front page, colourscheme, links, headers, formats etc.

--- StaticPages/urls.py
+++ StaticPages/urls.py
@@ -2,5 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# Homepage
 	path('', views.index, name='index'),
+
+	# About Section
+	path('About/Meets/', views.AboutMeetsFormatCost, name='AboutMeetsFormatCost'),
+	path('About/TackleStore/', views.AboutTackleStore, name='AboutTackleStore'),
+	path('About/Library/', views.AboutLibrary, name='AboutLibrary'),
+	path('About/Constition&Safety/', views.AboutConstitutionSafety, name='AboutConstitutionSafety'),
+
+	# Contact Section
+
+	# Gear Section
+
+	# Meets Section
+
+	# Get Involved Section
+
 ]
--- StaticPages/views.py
+++ StaticPages/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
+# Homepage
 def index(request):
     return render(request, 'StaticPages/index.html')
+
+# About Section
+def AboutMeetsFormatCost(request):
+    return render(request, 'StaticPages/About/MeetsFormatCost.html')
+
+def AboutTackleStore(request):
+    return render(request, 'StaticPages/About/TackleStore.html')
+
+def AboutLibrary(request):
+    return render(request, 'StaticPages/About/Library.html')
+
+def AboutConstitutionSafety(request):
+    return render(request, 'StaticPages/About/ConstitutionSafety.html')
+
+# Contact Section
+
+# Gear Section
+
+# Meets Section
+
+# Get Involved Section
