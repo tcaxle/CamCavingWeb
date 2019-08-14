0eefa35da26f2c31b1beafff9c9674ce5a7b3681
Tom Crossley <tcac2@cam.ac.uk>
2019-08-14 19:59:20 +0100

    changed static_url and static_root

--- CamCavingWeb/settings.py
+++ CamCavingWeb/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/home/tcac2/public_html/static/'
-STATIC_URL = '/static/'
+STATIC_ROOT = '/societies/caving/public_html/'
+STATIC_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "Static"),
     os.path.join(BASE_DIR, "Archive"),
