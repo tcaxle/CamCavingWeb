c834676e005c78f2669f3c743991643ac6b94f39
Tom Crossley <tcac2@cam.ac.uk>
2019-08-14 19:51:55 +0100

    Will deploy now.

--- CamCavingWeb/settings.py
+++ CamCavingWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ji7t3y^b!-a9(v*6(cj&+l-hs_f6ge0f1*_w8g#bf$xr*b#e5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [u'tcac2.user.srcf.net', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
