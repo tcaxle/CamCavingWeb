da3df974535e45c0fb3c5a5fa1ed82b77d7b37a9
Tom Crossley <tcrossley@SolarFlarecom.com>
2019-08-19 17:46:18 +0100

    added checkboxes to rank

--- UserPortal/forms.py
+++ UserPortal/forms.py
@@ -15,6 +15,8 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'full_name', 'email', 'mailing_list', 'phone_number', 'emergency_contact_name', 'emergency_phone_number', 'status', 'bio', 'tape_colour_1', 'tape_colour_2', 'tape_colour_3', 'tape_colour_notes')
 
 class CustomUserChangeForm(UserCreationForm):
+    rank = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
+
     class Meta(UserChangeForm):
         model = CustomUser
         fields = ('username', 'user_key', 'rank', 'full_name', 'email', 'mailing_list', 'phone_number', 'emergency_contact_name', 'emergency_phone_number', 'status', 'bio', 'tape_colour_1', 'tape_colour_2', 'tape_colour_3', 'tape_colour_notes')
