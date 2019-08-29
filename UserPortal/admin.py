bf5d7eddc8e5c4ea6f3d2ed936713794e87b5c2a
CU Caving Club <caving@sinkhole.srcf.net>
2019-08-29 11:55:38 +0100

    minor changes

--- UserPortal/admin.py
+++ UserPortal/admin.py
@@ -6,15 +6,6 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import *
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-            (None, {'fields': ('full_name', 'bio', 'tape_colour_1', 'tape_colour_2', 'tape_colour_3', 'tape_colour_notes')}),
-    )
-    list_display = ['username', 'full_name', 'email', 'bio']
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
 admin.site.register(Rank)
 admin.site.register(Committee)
