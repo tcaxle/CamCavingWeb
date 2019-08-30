ee462f1a9b8c0d775f588adb7f77b8559a38d577
Tom Crossley <tcrossley@SolarFlarecom.com>
2019-08-30 09:50:34 +0100

    ?/

--- Bank/models.py
+++ Bank/models.py
@@ -34,7 +34,7 @@ class Account(models.Model):
             ("view_own__account", "Can view own account"),
             ("view_other__account", "Can view others' accounts"),
         ]
-        ordering = ['sort_name']
+        ordering = ['type', 'sort_name']
 
 class CustomCurrency(models.Model):
     # Allows custom "currencies," which are rates to be charged
